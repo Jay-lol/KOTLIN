9db384b10cf00616cf8425ee245bb3f878c886c0
Jay-lol <krgeo@naver.com>
2020-04-18 19:02:17 +0900

    간단한 웹브라우저

---
+++ MyWebBrowser/app/src/main/java/practice/kotlin/com/mywebbrowser/MainActivity.kt
@@ -0,0 +1,113 @@
+package practice.kotlin.com.mywebbrowser
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.browse
+import org.jetbrains.anko.email
+import org.jetbrains.anko.sendSMS
+import org.jetbrains.anko.share
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+        }
+        webView.loadUrl("http://www.google.com")
+
+        urlEditText.setOnEditorActionListener{_,actionId,_ ->
+            if(actionId == EditorInfo.IME_ACTION_SEARCH){
+                webView.loadUrl(urlEditText.text.toString())
+                true
+            }else{
+                false
+            }
+
+        }
+
+        registerForContextMenu(webView)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.action_google, R.id.action_home ->{
+                webView.loadUrl("http://www.google.com")
+                return true
+            }
+            R.id.action_naver -> {
+                webView.loadUrl("http://www.naver.com")
+                return true
+            }
+            R.id.action_fow -> {
+                webView.loadUrl("http://www.fow.tv")
+                return true
+            }
+            R.id.action_call -> {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel: 010-1234-5678")
+                if(intent.resolveActivity(packageManager)!=null){
+                    startActivity(intent)
+                }
+                return true
+            }
+            R.id.action_send_text -> {
+                sendSMS("010-1234-5678",webView.url)
+                return true
+            }
+            R.id.action_email -> {
+                email("krgeo@naver.com", "좋은 사이트", webView.url)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId) {
+            R.id.action_share -> {
+                // 페이지 공유
+                share(webView.url)
+                return true
+            }
+            R.id.action_browser -> {
+                // 기본 웹브라우저에서 열기
+                browse(webView.url)
+                return true
+            }
+
+        }
+
+        return super.onContextItemSelected(item)
+    }
+}
